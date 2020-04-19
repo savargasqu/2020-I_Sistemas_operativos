b5c95737fc1701719c1cfeb16e8069512cb1d5f5
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-04-19 14:59:42 -0500

    fix struct generator

--- practica01/src/struct_generator.c
+++ practica01/src/struct_generator.c
@@ -1,7 +1,7 @@
 #include "../p1-dogProgram.h"
 
 int generate_random_int(int min, int max) {
-  return (rand() % (max - min + 1)) + min;
+  return (rand() % (max - min + 1)) + min; // range [min, max]
   //return rand() % max; // range [0, max)
 }
 
@@ -9,12 +9,14 @@ float generate_random_float(float max) {
   return ((float)rand() / (float)(RAND_MAX)) * max;
 }
 
-char *generate_random_string(int len) {
-  char *rstr = malloc((len + 1) * sizeof(char));
-  for (int i = 0; i < len; i++) {
-    rstr[i] = generate_random_int(97, 123); // ASCII a-z
+char *generate_random_string(int str_len) {
+  char *rstr = malloc((str_len) * sizeof(char));
+  int name_len = generate_random_int(1, str_len - 2);
+  int i;
+  for (i = 0; i < name_len; i++) {
+    rstr[i] = generate_random_int(97, 122); // ASCII a-z
   }
-  rstr[len] = '\0';
+  rstr[str_len - 1] = '\0';
   return rstr;
 }
 
@@ -28,15 +30,14 @@ char generate_random_gender() {
 
 struct dogType generate_dog() {
   struct dogType *dog_ptr = (struct dogType *)malloc(sizeof(struct dogType));
-
   strcpy(dog_ptr->name, generate_random_string(32));
   strcpy(dog_ptr->species, generate_random_string(32));
   strcpy(dog_ptr->breed, generate_random_string(16));
-  dog_ptr->age = generate_random_int(0, 100);
-  dog_ptr->height = generate_random_int(0, 100);
+  dog_ptr->age = generate_random_int(1, 100);
+  dog_ptr->height = generate_random_int(1, 100);
   dog_ptr->weight = generate_random_float(100);
   dog_ptr->sex = generate_random_gender();
-  dog_ptr->ID = -1; // Hash table will give the dog a proper ID
+  dog_ptr->ID = 0; // Hash table will give the dog a proper ID
 
   return *dog_ptr;
 }
@@ -46,9 +47,9 @@ Table generate_table() {
   Table ht = create_table();
 
   for (unsigned long i = 0; i < 10000000; i++) {
-    printf("%lu", i);
     struct dogType dog = generate_dog();
-    insert_to_table(ht, dog.name, dog);
+    unsigned long id = insert_to_table(ht, dog.name, dog);
+    printf("%lu: %s\n", id, dog.name);
   }
   return ht;
 }
