c863574fd012266181384d43caa51a87450e675c
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-05-30 17:27:20 -0500

    almost finished with table_generator. wait to update record definition

--- practica01/src/table_generator.c
+++ practica01/src/table_generator.c
@@ -1,5 +1,6 @@
 #include "../p1-dogProgram.h"
-/* RANDOMIZATION MODULE */
+
+/* Randomization module. Functions that generate different random data types */
 
 /* generate_random_table: Takes a table and fills it with random records */
 void generate_random_table(table_t *table_ptr) {
@@ -49,16 +50,18 @@ char generate_random_gender() {
   return 'M';
 }
 
-// struct dogType generate_dog() {
-//  struct dogType *dog_ptr = (struct dogType *)malloc(sizeof(struct dogType));
-//  strcpy(dog_ptr->name, generate_random_string(32));
-//  strcpy(dog_ptr->species, generate_random_string(32));
-//  strcpy(dog_ptr->breed, generate_random_string(16));
-//  dog_ptr->age = generate_random_int(1, 100);
-//  dog_ptr->height = generate_random_int(1, 100);
-//  dog_ptr->weight = generate_random_float(100);
-//  dog_ptr->sex = generate_random_gender();
-//  dog_ptr->ID = 0; // Hash table will give the dog a proper ID
-//
-//  return *dog_ptr;
-//}
+/*
+dogType generate_dog() {
+  dogType *dog_ptr = allocate_record();
+  strcpy(dog_ptr->name, generate_random_string(32));
+  strcpy(dog_ptr->species, generate_random_string(32));
+  strcpy(dog_ptr->breed, generate_random_string(16));
+  dog_ptr->age = generate_random_int(1, 100);
+  dog_ptr->height = generate_random_int(1, 100);
+  dog_ptr->weight = generate_random_float(100);
+  dog_ptr->sex = generate_random_gender();
+  //dog_ptr->ID = 0; // This isn't valid any more
+
+  return *dog_ptr;
+}
+*/
